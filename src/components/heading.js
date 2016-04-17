0b86c99ecb3ee19d29b2faafef1cbd67b33ecede
github username <github email address>
2016-04-17 17:35:03 -0400

    Initial Commit

---
+++ src/components/heading.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Heading extends Component {
+
+  render() {
+    return (
+      <div>
+      <h1 className="heading">You Tube React Search</h1>
+      <p className="heading">Search for anything and see the results rendered in React.js</p>
+      </div>
+  )
+  }
+
+}
+
+
+export default Heading;
