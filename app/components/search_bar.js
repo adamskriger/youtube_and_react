b39d72c611ee46469631a2b7706a6d181cf9825c
github username <github email address>
2016-05-06 14:09:57 -0400

    styling for mobile V

--- app/components/search_bar.js
+++ app/components/search_bar.js
@@ -12,15 +12,13 @@ class SearchBar extends Component {
 
   render() {
 
-    
+
 
     return (
-    <div >
     <input
       className="search-bar"
       value={this.state.term}
       onChange={event => this.onInputChange(event.target.value)} />
-    </div>
   );
   }
 
