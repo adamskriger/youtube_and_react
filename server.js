7294ca0bfb5f6e2128f4b0aa271fcf670832ef39
github username <github email address>
2016-05-06 10:54:37 -0400

    Changed directory structure in prep for deployment

---
+++ server.js
@@ -0,0 +1,25 @@
+/* Global Requires */
+
+const express    = require('express');
+const path       = require('path');
+const app        = express();
+
+
+if(process.env.NODE_ENV === 'development') {
+  console.log('in development.');
+  require('dotenv').config();
+} else {
+  console.log('in production.');
+}
+
+/* App Config */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(logger('dev'));
+/* Server Initialization */
+app.get('/', (req, res) => res.sendFile('index.html'));
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server initialized on // ${new Date()}`));
+
+//new configs//
